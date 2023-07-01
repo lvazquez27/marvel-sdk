6d683990a80df69fc6c271b2c26074b879428c49
Luz Vazquez <vzulvazquez@gmail.com>
2023-06-30 21:03:51 -0600

    add main class

---
+++ src/main/java/mx/openpay/marvel_sdk/MarvelSdkApplication.java
@@ -0,0 +1,8 @@
+package mx.openpay.marvel_sdk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarvelSdkApplication {
+}
